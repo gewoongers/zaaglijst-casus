b924fcb0bdc9c8bfe24c6ed9b0a258504a04b991
Anthenny <anthenny1997@hotmail.com>
2023-03-09 00:20:58 +0100

    We ontvangen de jsondata en dumpen het gewensten resultaat op het scherm zie screenshot

--- app/Http/Controllers/ProductionStateController.php
+++ app/Http/Controllers/ProductionStateController.php
@@ -4,8 +4,53 @@ namespace App\Http\Controllers;
 
 class ProductionStateController extends Controller
 {
-    public function index(): string
+    public function index()
     {
-        return file_get_contents(storage_path() . "/data/ProductieStaat.json");
+        $data = json_decode(file_get_contents(storage_path() . "/data/ProductieStaat.json"), true);
+        
+        // filter voor individueel saw object
+        foreach($data as $part) {
+            $saws[] = $part['saw'];
+            // filter voor de unique titels en stop het in een array
+            foreach($saws as $saw) {
+                if(isset($saw['profielkleur']['title'])) {
+                    $title = $saw['profielkleur']['title'];
+                    $uniqueTitles[] = $saw['profielkleur']['title'];
+                }
+            }
+        }
+        
+        $uniqueTitles = array_unique($uniqueTitles);
+        $uniqueTitles = array_values($uniqueTitles);
+
+        // pak een individueel saw object
+        foreach($saws as $saw => $profielen) {
+            // filter alle arrays op G(nummer)
+            $filteredSubArrays = array_filter($profielen, function($key) {
+                return preg_match('/^.*g\d+$/i', $key);
+
+            }, ARRAY_FILTER_USE_KEY);
+
+            // Geef de profielen een andere naam
+            foreach($filteredSubArrays as $key => $subArray) {
+                preg_match('/g(\d+)/i', $key, $matches);
+                $renamedSubArrays['G'. $matches[1]] = ['length' => $subArray['value'] . ' mm', 'count' => $subArray['amount']];
+            }
+
+            foreach($uniqueTitles as $title) {
+                if(isset($profielen['profielkleur']['title'])) {
+                    if($profielen['profielkleur']['title'] === $title) {
+                        $uniqueTitlesAssoc[$title] = $renamedSubArrays;
+                    }
+                }
+            }
+        }
+
+        dd($uniqueTitlesAssoc);
+
+        return view('decoded', [
+            'decoded' => $uniqueTitlesAssoc
+        ]);
+
     }
 }
